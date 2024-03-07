68d90bf3538d976784d68c3361bde81398a1c1d8
moonbeam5115 <fabritecht@gmail.com>
2024-03-06 21:56:06 -0500

    update code

--- experimento_calculo_de_caudal.py
+++ experimento_calculo_de_caudal.py
@@ -3,7 +3,7 @@ from pyfirmata import util
 import win32api
 import time
 
-state_left = win32api.GetKeyState(0x01)  # Left button up = 0 or 1. Button down = -127 or -128
+state_right = win32api.GetKeyState(0x02)  # Right button up = 0 or 1. Button down = -127 or -128
 
 board = pyfirmata.Arduino('COM3')
 
@@ -25,20 +25,23 @@ volumen_experimental = 1
 digital_5_output.write(0)
 
 print("Comenzando Experimento Para Determinar Caudal...")
+i = 0
 while True:
     humedad_sensor_tierra = analog_0_input.read()
     
     print("output 5: ", digital_5_output)
-    a = win32api.GetKeyState(0x01)
-    if a != state_left:  # Button state changed
-        state_left = a
-        
-        if a < 0:
+    a = win32api.GetKeyState(0x02)
+    if a < 0:  # Button state changed
+        if i == 0:
+            i += 1
             comienzo = time.time()
-            digital_5_output.write(1)
-        else:
-            final = time.time()
-            digital_5_output.write(0)
-            cambio_en_tiempo = final - comienzo
-            caudal = volumen_experimental/cambio_en_tiempo
-            print(f"El Caudal es: {caudal} L/s")
+            
+        digital_5_output.write(1)
+    else:
+        final = time.time()
+        digital_5_output.write(0)
+        cambio_en_tiempo = final - comienzo
+        caudal = volumen_experimental/cambio_en_tiempo
+        print(f"El Caudal es: {caudal} L/s")
+        exit(0)
+        
