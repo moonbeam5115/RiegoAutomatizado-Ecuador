b6850dd1f3ba5d5bcee49fa7adf8d5ab39946250
moonbeam5115 <fabritecht@gmail.com>
2024-05-14 21:27:22 -0400

    subir apagar_outputs_arduino.ino

--- Unused_data/apagar_outputs_arduino.ino
+++ Unused_data/apagar_outputs_arduino.ino
@@ -2,7 +2,6 @@
 
 int OUT5 = 5;
 int OUT6 = 6;
-int Pin1 = A0; 
 
 const byte relayOnState = LOW;
 const byte relayOffState = HIGH;
@@ -10,9 +9,12 @@ const byte relayOffState = HIGH;
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
+
+    // Set los variables OUT5 y OUT6 que sean OUTPUT
     pinMode(OUT5, OUTPUT);
     pinMode(OUT6, OUTPUT);
 
+    // Escribir (mandar o cortal senal) al ARDUINO
     digitalWrite(OUT5, relayOffState);
     digitalWrite(OUT6, relayOffState);
     delay(500);
